5f0327de306b16f82edc98dfabf3b7418db61afd
val09072010 <val09072010@gmail.com>
2019-08-30 17:49:28 +0300

    pylint corrections. p3

--- exporters/plain_text_exporter.py
+++ exporters/plain_text_exporter.py
@@ -5,12 +5,12 @@ class PlainTextExporter(GenericExporter):
     def _export_to_output_file(self, tickets, milestones):
         i = 1
         res = []
-        for item in tickets:
-            # TODO 1: make field configurable i.e. item.key or item.fields.summary etc.
-            item_milestones = list(map(lambda x: x.format(item.fields.summary, i), milestones))
+        for tck in tickets:
+            # TODO 1: make field configurable i.e. tck.key or tck.fields.summary etc.
+            tck_milestones = list(map(lambda x, it=tck: x.format(it.fields.summary, i), milestones))
             # TODO 2: replace append with write
             with open(self.output_file, "at", encoding=self.out_encoding) as out:
-                out.writelines(item_milestones)
-            res.append(item_milestones)
+                out.writelines(tck_milestones)
+            res.append(tck_milestones)
             i += 1
         return res
