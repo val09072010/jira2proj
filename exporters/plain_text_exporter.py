e8b43e16b40a5307c56de5251305f9639d09d0a3
val <val09072010@gmail.com>
2019-10-12 12:44:59 +0300

    feedback usage update

--- exporters/plain_text_exporter.py
+++
@@ -1,8 +0,0 @@
-from exporters.generic_exporter import GenericExporter
-
-
-class PlainTextExporter(GenericExporter):
-    def _export_to_output_file(self, project_tasks):
-        with open(self.output_file, "wt", encoding=self.out_encoding) as out:
-            doc_len = out.writelines(project_tasks)
-        return doc_len
--- jira2proj.py
+++ jira2proj.py
@@ -7,12 +7,147 @@ Output: xml file with tasks and resources
 import sys
 import getopt
 import config
-from issue_trackers.jira_connector import JiraConnector
-from exporters.xml_exporter import XmlExporter
-from exporters.plain_text_exporter import PlainTextExporter
+from lxml import etree as et
+from jira.client import JIRA
+import urllib3
 
 
-TEST_ITEMS = ["Test task 1", "Test task 2", "Задача на русском 3"]
+TEMPLATE_PROJ_XML = 'assets/template.xml'
+TASKS_TAG = '{http://schemas.microsoft.com/project}Tasks'
+CALENDAR_TAG = '{http://schemas.microsoft.com/project}Calendars'
+TASKS_TAG_NO_NAMESPACE = "Tasks"
+
+PROJ_TAGS_MAP = {'Name': 'Template', 'Title': 'Template', 'Manager': 'PM', 'ScheduleFromStart': '1',
+                 'StartDate': '2019-09-30T08:00:00', 'FinishDate': '2019-10-01T17:00:00', 'FYStartDate': '1',
+                 'CriticalSlackLimit': '0', 'CurrencyDigits': '2', 'CurrencySymbol': '$', 'CurrencySymbolPosition': '0',
+                 'CalendarUID': '1', 'DefaultStartTime': '11:00:00', 'DefaultFinishTime': '20:00:00',
+                 'MinutesPerDay': '480', 'MinutesPerWeek': '2400', 'DaysPerMonth': '20', 'DefaultTaskType': '0',
+                 'DefaultFixedCostAccrual': '2', 'DefaultStandardRate': '10', 'DefaultOvertimeRate': '15',
+                 'DurationFormat': '7', 'WorkFormat': '2', 'EditableActualCosts': '0', 'HonorConstraints': '0',
+                 'EarnedValueMethod': '0', 'InsertedProjectsLikeSummary': '0', 'MultipleCriticalPaths': '0',
+                 'NewTasksEffortDriven': '0', 'NewTasksEstimated': '1', 'SplitsInProgressTasks': '0',
+                 'SpreadActualCost': '0', 'SpreadPercentComplete': '0', 'TaskUpdatesResource': '1',
+                 'FiscalYearStart': '0', 'WeekStartDay': '1', 'MoveCompletedEndsBack': '0',
+                 'MoveRemainingStartsBack': '0', 'MoveRemainingStartsForward': '0', 'MoveCompletedEndsForward': '0',
+                 'BaselineForEarnedValue': '0', 'AutoAddNewResourcesAndTasks': '1',
+                 'CurrentDate': '2019-09-29T11:05:00', 'MicrosoftProjectServerURL': '1', 'Autolink': '1',
+                 'NewTaskStartDate': '0', 'DefaultTaskEVMethod': '0', 'ProjectExternallyEdited': '0',
+                 'ActualsInSync': '0', 'RemoveFileProperties': '0', 'AdminProject': '0'}
+
+TASK_TAGS = {'Priority': '500', 'Start': '2019-10-02T08:00:00',
+             'Finish': '2019-10-03T17:00:00', 'Duration': 'PT8H0M0S', 'DurationFormat': '7', 'Work': 'PT08H0M0S',
+             'EffortDriven': '1', 'Estimated': '1', 'FixedCostAccrual': '2', 'ConstraintType': '0', 'CalendarUID': '-1',
+             'ConstraintDate': '1970-01-01T00:00:00', 'IgnoreResourceCalendar': '0'}
+
+DEF_TASK_ID = 1
+DEF_TASK_UID = 1
+
+
+class JiraConnector:
+    __slots__ = ['options', 'login', 'password']
+
+    def __init__(self, options, login, password):
+        self.options = options
+        self.login = login
+        self.password = password
+
+    def get_items(self, jira_jql_statemet, fields):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        jira_con = JIRA(self.options, basic_auth=(self.login, self.password))
+        items = jira_con.search_issues(jira_jql_statemet, fields=fields)
+        jira_con.close()
+        # TODO 1: make field configurable i.e. tck.key or tck.fields.summary etc.
+        # TODO 2: IMPLICIT dependency! Decouple code and assets/milestones.txt
+        return list(map(lambda s: s.fields.summary, items))
+
+
+class GenericExporter:
+    __slots__ = ['milestones_file', 'output_file', 'out_encoding']
+
+    def __init__(self, out_file, milestones, encoding):
+        self.output_file = out_file
+        self.milestones_file = milestones
+        self.out_encoding = encoding
+
+    def _export_to_output_file(self, project_tasks):
+        """the actual method for exporting milestones"""
+        raise NotImplementedError("Please use concrete child class")
+
+    def _read_milestones(self):
+        with open(self.milestones_file, "r") as mls:
+            milestones = mls.readlines()
+        return milestones
+
+    def export(self, tickets):
+        if not tickets:
+            print("No tickets to export please check the FILTER settings in config_local.py")
+            exit(-1)
+
+        milestones = self._read_milestones()
+        i = 1
+        project_tasks = []
+        for tck in tickets:
+            tck_milestones = list(map(lambda x, it=tck: x.format(it, i), milestones))
+            project_tasks.extend(tck_milestones)
+            i += 1
+        return self._export_to_output_file(list(map(lambda x: x.strip(), project_tasks)))
+
+
+class PlainTextExporter(GenericExporter):
+    def _export_to_output_file(self, project_tasks):
+        with open(self.output_file, "wt", encoding=self.out_encoding) as out:
+            doc_len = out.writelines(project_tasks)
+        return doc_len
+
+
+class XmlExporter(GenericExporter):
+    def _export_to_output_file(self, project_tasks):
+
+        root = self.__generate_xml_template()
+        tasks = root.find(TASKS_TAG)
+
+        if tasks is None:
+            tasks = et.SubElement(root, TASKS_TAG_NO_NAMESPACE)
+
+        task_uid = DEF_TASK_UID
+        task_id = DEF_TASK_ID
+
+        for raw_task in project_tasks:
+            task = et.SubElement(tasks, "Task")
+            self.__insert_sub_element(task, "Name", raw_task)
+            self.__insert_sub_element(task, "UID", str(task_uid))
+            self.__insert_sub_element(task, "ID", str(task_id))
+            for k in TASK_TAGS:
+                self.__insert_sub_element(task, k, TASK_TAGS[k])
+            task_id += 1
+            task_uid += 1
+
+        et.SubElement(root, "Resources")
+        et.SubElement(root, "Assignments")
+
+        with open(self.output_file, "w", encoding=self.out_encoding) as out:
+            out_str =  str(et.tostring(root, pretty_print=True, encoding=self.out_encoding), encoding=self.out_encoding)
+            doc_len = out.write(out_str)
+
+        return doc_len
+
+    def __generate_xml_template(self):
+        # Step 1: add <Project>
+        root = et.Element("Project", xmlns="http://schemas.microsoft.com/project")
+        # Step 2: add mandatory tags
+        for k in PROJ_TAGS_MAP:
+            self.__insert_sub_element(root, k, PROJ_TAGS_MAP[k])
+        # Step 3: add Calendar
+        print(str(et.tostring(root, encoding=self.out_encoding, pretty_print=True), encoding=self.out_encoding))
+
+        return root
+
+    @staticmethod
+    def __insert_sub_element(root, tag, value):
+        sub_element = et.SubElement(root, tag)
+        if value:
+            sub_element.text = value
+        return sub_element
 
 
 def main(argv):
@@ -20,18 +155,20 @@ def main(argv):
     milestones_file = config.MILESTONES_FILES
     to_plain_text = False
     with_jira = True
+    tasks_from_text_file = ""
 
     try:
-        opts, _ = getopt.getopt(argv, "o:m:f:t:n:", ["output=", "ms=", "text=", "no-jira="])
+        opts, _ = getopt.getopt(argv, "o:m:f:t:n:", ["output=", "milestones=", "text=", "no-jira="])
         for opt, arg in opts:
             if opt in ("-o", "--output"):
                 resulting_file = arg
-            elif opt in ("-m", "--ms"):
+            elif opt in ("-m", "--milestones"):
                 milestones_file = arg
             elif opt in ("-t", "--text"):
                 to_plain_text = True
             elif opt in ("-n", "--no-jira"):
                 with_jira = False
+                tasks_from_text_file = arg
             else:
                 pass
     except getopt.GetoptError:
@@ -45,7 +182,11 @@ def main(argv):
         # 2. get list of JIRA Stories, Epics, Features (i.e. subjects of delivery)
         items = jira_con.get_items(config.JIRA_FILTER, config.JIRA_FIELDS)
     else:
-        items = TEST_ITEMS
+        if tasks_from_text_file:
+            with open(tasks_from_text_file, "r") as f:
+                items = f.readlines()
+        else:
+            items = []
     # 3. generate XML file
     # 4. get milestones list and apply it to each item from (2)
     if to_plain_text:
